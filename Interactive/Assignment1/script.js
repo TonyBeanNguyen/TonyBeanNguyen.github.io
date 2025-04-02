b0652ed6bd10b4745351f021020dfbc6bdd90d52
TonyBeanNguyen <168186257+TonyBeanNguyen@users.noreply.github.com>
2025-04-02 22:57:27 +1100

    Changes with 3D

---
+++ Interactive/Assignment1/script.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("img1").addEventListener("mouseover", function () {
+    this.src = "chair.png";
+});
+
+document.getElementById("img1").addEventListener("mouseout", function () {
+    this.src = "chairfloat.png";
+});
